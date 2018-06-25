5ead06fc9f887a0b9634f60878197c5104b7658b
ThiagoASilva <thiago.afonso2010@hotmail.com>
2018-06-24 22:31:10 -0300

    Primeiro commit da aplicação Android. Conectando-se ao Broker MQTT e enviando comandos no Topico designado

---
+++ app/src/main/java/tcc/tavcompany/energycontrol/Metas.java
@@ -0,0 +1,187 @@
+package tcc.tavcompany.energycontrol;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.util.Log;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.Toast;
+
+import org.eclipse.paho.android.service.MqttAndroidClient;
+import org.eclipse.paho.client.mqttv3.IMqttActionListener;
+import org.eclipse.paho.client.mqttv3.IMqttToken;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+
+public class Metas extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    static String MQTTHOST = "tcp://192.168.0.18:1883";
+    static String USERNAME = "systemem";
+    static String PASSWORD = "kwhy123";
+    String Topico = "TCCCOMANDO";
+    String clientId = "AplicacaoAndroid";
+
+    MqttAndroidClient client;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_metas);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Colocar funções para o Energy Control", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        //Botoes---------------------------------------
+
+
+        Switch LigarESP = (Switch) findViewById(R.id.switchESP1);
+        LigarESP.setChecked(true);
+
+        LigarESP.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if(compoundButton.isChecked()){
+
+                    Toast.makeText(Metas.this, "ESPTomada1 Ligado", Toast.LENGTH_SHORT).show();
+                    publicar();
+                }else{
+
+                    Toast.makeText(Metas.this, "ESPTomada1 Desligado", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+
+        //MQTT----------------------------------------
+
+
+        client = new MqttAndroidClient(this.getApplicationContext(), MQTTHOST, clientId);
+
+        MqttConnectOptions options = new MqttConnectOptions();
+
+        options.setUserName(USERNAME);
+        options.setPassword(PASSWORD.toCharArray());
+
+        try {
+            IMqttToken token = client.connect(options);
+            token.setActionCallback(new IMqttActionListener() {
+                @Override
+                public void onSuccess(IMqttToken asyncActionToken) {
+                    Toast.makeText(Metas.this,"MQTT conectado!", Toast.LENGTH_LONG).show();                }
+
+                @Override
+                public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+                    // Something went wrong e.g. connection timeout or firewall problems
+                    Toast.makeText(Metas.this, "MQTT Falhou ao conectar!", Toast.LENGTH_LONG).show();
+                    Log.e("CONEXAO BROKER MQTT", exception.getMessage());
+                }
+            });
+        } catch (MqttException e) {
+            e.printStackTrace();
+            Log.e("Erro", e.getMessage());
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.metas, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_metas) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    public void ligarDispositivo(String dispositivo){
+
+        String comando = "Aplicativo/comando/ligar_"+dispositivo;
+
+    }
+
+    public void publicar(){
+        String topic = Topico;
+        String mensagem = clientId+"/comando_dispositivo/ESPTOMADA1%ligar-desligar";
+        try {
+            client.publish(topic, mensagem.getBytes(), 0, false);
+        } catch (MqttException e) {
+            e.printStackTrace();
+        }
+    }
+}
